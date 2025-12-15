642f7b32670e2a24b25c395e63ed0043c385e7b5
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2025-12-15 02:14:31 +0000

    01_VarsAndOps

---
+++ 01_var_ops/var.js
@@ -0,0 +1,7 @@
+var x;
+x=1;
+console.log(x);
+
+let xx =1;
+console.log(xx);
+
---
+++ 01_var_ops/var.ts
@@ -0,0 +1,8 @@
+let fname: string = "Luiz";
+console.log("Name:",fname);
+
+let age: number = 33;
+console.log("Age:",age);
+
+const pi: number = 3.14;
+console.log("Pi:",pi);
